978ce3104d8fd8dabbc65a12b38c341e72d48bb8
Jordan Carman <jordancarman9@gmail.com>
2018-02-28 11:40:11 +0000

    Got missed platform detection to work
    Game ends when player misses platform, lives would be difficult to implement with the current method, and may not actually work at all with this method.
    
    Brief playtesting suggested that lives are unnecessary so unless you guys really want them I'm not putting them in.

--- MGP_Group1/Assets/Scripts/PointCollection.cs
+++ MGP_Group1/Assets/Scripts/PointCollection.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PointCollection : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
